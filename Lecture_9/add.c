13e1ef573f153f6cc274d9ac63e36f214e4013ef
Jason Jones <jasojone@gmail.com>
2022-02-21 16:45:17 -0800

    adds new files from lecture 9

---
+++ Lecture_9/add.c
@@ -0,0 +1,9 @@
+#include "header.h" //will include all variables and functions in header 
+
+//Let's define the function that was a prototype in header.h 
+
+int add(int a, int b){ 
+
+    return a + b; 
+
+} 
---
+++ Lecture_9/header.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_H   
+
+/* If multiple files have include header it will only be read once by compiler */ 
+
+#define HEADER_H 
+
+int add(int a, int b);  /* An example function declaration */ 
+
+#endif  
