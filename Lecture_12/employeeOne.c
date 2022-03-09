017544a994458590f1b9957daf714944304f6860
Jason Jones <jasojone@gmail.com>
2022-03-08 20:02:26 -0800

    work from live coding lecture 12

---
+++ Lecture_12/employeeOne.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include "employee.h"
+
+PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int tableSize, long targetNumber)
+{
+
+    const PtrToConstEmployee endPtr = ptr + tableSize;
+
+    for (; ptr < endPtr; ptr++) // search until end of table  ptr++ will increment by what??
+
+    {
+        if (ptr->number == targetNumber) // check if it equals the Employee number
+            return (PtrToEmployee)ptr;   // return location of the number to callee.
+    }
+    return NULL; // this will only happen if no Employee number matches in loop above
+}
+
+// Essentially the same functionality as above but comparing strings to check if equal
+
+PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char *targetName)
+{
+
+    const PtrToConstEmployee endPtr = ptr + tableSize;
+
+    for (; ptr < endPtr; ptr++)
+    {
+        if (strcmp(ptr->name, targetName) == 0)
+            return (PtrToEmployee)ptr;
+    }
+
+    return NULL;
+}
---
+++ Lecture_12/employeeTable.c
@@ -0,0 +1,17 @@
+#include <string.h> 
+#include <stdlib.h> 
+#include "employee.h" 
+
+Employee EmployeeTable[] =  
+
+{ 
+
+    {1001, "Daphne Borromeo",   "909-555-2134", 8.78}, 
+    {1011, "Tammy Franklin",    "213-555-1212", 4.50}, 
+    {1140, "Dorris Perl",       "310-555-1215", 7.80}, 
+    {4011, "Tony Bobcat",       "909-555-1235", 6.34}, 
+    {5045, "Brian Height",      "714-555-2749", 8.32} 
+
+}; 
+
+const int EmployeeTableEntries = sizeof(EmployeeTable)/sizeof(EmployeeTable[0]); 
